d418e790cf1afdc335164f99e9dfee1a4320fb9c
Mateusz Tuński <mateusz@tunski.net>
2014-09-19 18:26:10 +0200

    Small formatting fixes

--- tests.rb
+++ tests.rb
@@ -10,14 +10,14 @@ class MoneyTest < Minitest::Test
   end
 
   def test_money_initialization
-    assert_equal 10, @money.value
+    assert_equal 10,    @money.value
     assert_equal 'USD', @money.currency
   end
 
   def test_money_shorthand_initialization
     money = Money(20, 'PLN')
 
-    assert_equal 20, money.value
+    assert_equal 20,    money.value
     assert_equal 'PLN', money.currency
   end
 
@@ -30,8 +30,8 @@ class MoneyTest < Minitest::Test
   end
 
   def test_money_from_currency
-    assert_equal "#<Money 10.00 USD>", Money.from_usd(10).inspect
-    assert_equal "#<Money 10.00 EUR>", Money.from_eur(10).inspect
-    assert_equal "#<Money 10.00 GBP>", Money.from_gbp(10).inspect
+    assert_equal '#<Money 10.00 USD>', Money.from_usd(10).inspect
+    assert_equal '#<Money 10.00 EUR>', Money.from_eur(10).inspect
+    assert_equal '#<Money 10.00 GBP>', Money.from_gbp(10).inspect
   end
 end
