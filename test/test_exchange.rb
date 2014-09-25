e532cfe78ee8d47adcc0c2b06a3947eaeef23841
Mateusz Tuński <mateusz@tunski.net>
2014-09-25 18:31:07 +0200

    Fix Exchange tests after code changes

--- test/test_exchange.rb
+++ test/test_exchange.rb
@@ -6,51 +6,47 @@ class ExchangeTest < Minitest::Test
     @exchange = Exchange.new
   end
 
-  def teardown
-  end
-
   def test_exchange_convert
-    mock_rates(@exchange)
+    stub_exchange_rates(@exchange)
 
-    assert_equal 12.25332524613867, @exchange.convert(Money(20, 'USD'), 'GBP').to_f
-    assert_equal 83.7204,           @exchange.convert(Money(20, 'EUR'), 'PLN').to_f
-    assert_equal 0,                 @exchange.convert(Money(0,  'EUR'), 'PLN').to_f
-    assert_equal 1,                 @exchange.convert(Money(1,  'EUR'), 'EUR').to_f
+    assert_equal 12.253325246138672, @exchange.convert(Money(20, 'USD'), 'GBP').value.to_f
+    assert_equal 83.7204,            @exchange.convert(Money(20, 'EUR'), 'PLN').value.to_f
+    assert_equal 0,                  @exchange.convert(Money(0,  'EUR'), 'PLN').value.to_f
+    assert_equal 1,                  @exchange.convert(Money(1,  'EUR'), 'EUR').value.to_f
   end
 
   def test_exchange_convert_raises_exception_with_appropriate_message_when_currency_is_invalid
-    mock_rates(@exchange)
+    stub_exchange_rates(@exchange)
 
-    err = assert_raises Exchange::InvalidCurrency do @exchange.convert(Money(20, 'GBP'), '') end
+    err = assert_raises Exchange::InvalidCurrencyError do @exchange.convert(Money(20, 'GBP'), '') end
     assert_equal 'Please, provide both currencies', err.message
 
-    err = assert_raises Exchange::InvalidCurrency do @exchange.convert(Money(20, ''),    'GBP') end
+    err = assert_raises Exchange::InvalidCurrencyError do @exchange.convert(Money(20, ''),    'GBP') end
     assert_equal 'Please, provide both currencies', err.message
 
-    err = assert_raises Exchange::InvalidCurrency do @exchange.convert(Money(20, 'GBP'), 'AAA') end
+    err = assert_raises Exchange::InvalidCurrencyError do @exchange.convert(Money(20, 'GBP'), 'AAA') end
     assert_equal 'Invalid currency: AAA', err.message
 
-    err = assert_raises Exchange::InvalidCurrency do @exchange.convert(Money(20, 'AAA'), 'GBP') end
+    err = assert_raises Exchange::InvalidCurrencyError do @exchange.convert(Money(20, 'AAA'), 'GBP') end
     assert_equal 'Invalid currency: AAA', err.message
 
-    err = assert_raises Exchange::InvalidCurrency do @exchange.convert(Money(20, 'AAA'), 'AAA') end
+    err = assert_raises Exchange::InvalidCurrencyError do @exchange.convert(Money(20, 'AAA'), 'AAA') end
     assert_equal 'Invalid currency: AAA', err.message
 
-    err = assert_raises Exchange::InvalidCurrency do @exchange.convert(Money(20, 'AAA'), 'ZZZ') end
+    err = assert_raises Exchange::InvalidCurrencyError do @exchange.convert(Money(20, 'AAA'), 'ZZZ') end
     assert_equal 'Invalid currency: AAA', err.message
   end
 
   def test_exchange_convert_raises_exception_with_appropriate_message_when_rates_not_fetched
-    err = assert_raises Exchange::RatesMissingError do @exchange.convert(Money(20, 'GBP'), '') end
-    assert_equal 'You have to fetch the conversion rates before converting!', err.message
+    err = assert_raises Exchange::RateMissingError do @exchange.convert(Money(20, 'GBP'), 'PLN') end
+    assert_equal 'You have to fetch the conversion rate before converting!', err.message
   end
 
-  def test_exchange_fetch_rates
-    assert_nil @exchange.rates
+  def test_exchange_fetch_rate
+    assert_empty @exchange.rates
 
-    @exchange.fetch_rates
+    @exchange.fetch_rate('USD', 'GBP')
 
-    assert_kind_of Hash, @exchange.rates
-    assert_equal   21,   @exchange.rates.size
+    assert_equal 2, @exchange.rates.size
   end
 end
